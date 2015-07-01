4a2c5b149a4fd6f4d2e2b7bd4cfdf6d979c72c27
daniel <dandromeda@hotmail.com>
2015-07-01 00:11:10 -0500

    Con ViewData

--- MVCDay1/Controllers/TestController.cs
+++ MVCDay1/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCDay1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,12 @@ namespace MVCDay1.Controllers
         }
         public ActionResult GetView()
         {
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+
+            ViewData["Employee"] = emp;
             return View("MyView");
         }
     }
