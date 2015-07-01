10293db88926aab59015141c1010aab7feea3636
daniel <dandromeda@hotmail.com>
2015-06-30 23:37:52 -0500

    Fin del Día 1

---
+++ MVCDay1/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDay1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
