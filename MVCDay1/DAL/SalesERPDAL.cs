4fba1260c2bd05c32c8983499fab8a116f10a570
daniel <dandromeda@hotmail.com>
2015-07-02 18:09:56 -0500

    Ultima prueba

---
+++ MVCDay1/DAL/SalesERPDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MVCDay1.Models;
+
+namespace MVCDay1.DAL
+{
+    public class SalesERPDAL : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("TblEmployee");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- MVCDay1/Models/Employee.cs
+++ MVCDay1/Models/Employee.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCDay1.Models
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Salary { get; set; }
